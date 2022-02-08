127615b8159d2ac325a543c16f137edc0f48f8c0
John Oseni <johnoseni26@gmail.com>
2022-02-08 20:34:17 +0100

    index.js file

--- pages/index.js
+++ pages/index.js
@@ -15,3 +15,61 @@ const style = {
   button: `border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
   details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
 }
+
+
+export default function Home() {
+  const { address, connectWallet } = useWeb3()
+
+  const welcomeUser = (userName, toastHandler = toast) => {
+    toastHandler.success(
+      `Welcome back${userName !== 'Unnamed' ? ` ${userName}` : ''}!`,
+      {
+        style: {
+          background: '#04111d',
+          color: '#fff',
+        },
+      }
+    )
+  }
+
+  useEffect(() => {
+    if (!address) return
+    ;(async () => {
+      const userDoc = {
+        _type: 'users',
+        _id: address,
+        userName: 'Unnamed',
+        walletAddress: address,
+      }
+
+      const result = await client.createIfNotExists(userDoc)
+
+      welcomeUser(result.userName)
+    })()
+  }, [address])
+
+  return (
+    <div className={style.wrapper}>
+      <Toaster position="top-center" reverseOrder={false} />
+      {address ? (
+        <>
+          <Header />
+          <Hero />
+        </>
+      ) : (
+        <div className={style.walletConnectWrapper}>
+          <button
+            className={style.button}
+            onClick={() => connectWallet('injected')}
+          >
+            Connect Wallet
+          </button>
+          <div className={style.details}>
+            You need Chrome to be
+            <br /> able to run this app.
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
