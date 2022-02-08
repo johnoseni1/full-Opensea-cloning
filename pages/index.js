dc1b5f575fd0d442111ccc2e9f22690fd71aed6c
John Oseni <johnoseni26@gmail.com>
2022-02-08 20:30:47 +0100

    index.js file

--- pages/index.js
+++ pages/index.js
@@ -5,4 +5,4 @@ import { useWeb3 } from '@3rdweb/hooks';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import toast, { Toaster } from 'react-hot-toast';
-import { client } from '../lib/sanityClient'
+import { client } from '../lib/sanityClient';
