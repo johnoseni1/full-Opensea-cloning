c465fa1db04b2a3c1496b4b2e59ce4e04273a82c
John Oseni <johnoseni26@gmail.com>
2022-02-08 20:29:05 +0100

    index.js file

--- pages/index.js
+++ pages/index.js
@@ -2,3 +2,5 @@
 import React, {useEffect} from 'react';
 import Head from 'next/head';
 import { useWeb3 } from '@3rdweb/hooks';
+import Header from '../components/Header';
+import Hero from '../components/Hero'
