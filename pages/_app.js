8978ee99d23dd234d2006d4fe86b2bbfc2af3539
John Oseni <johnoseni26@gmail.com>
2022-02-08 20:53:07 +0100

    index.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
+import '../styles/globals.css'
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks'
 
-import { ThirdwebWeb3Provider } from '@3rdweb/hooks';
-import '../styles/globals.css';
-
-
+/**
+ * The chain ID 4 represents the Rinkeby network
+ * The `injected` connector is a web3 connection method used by Metamask
+ */
 const supportedChainIds = [4]
 const connectors = {
   injected: {},
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,17 @@
-
-import React, {useEffect} from 'react';
-import Head from 'next/head';
-import { useWeb3 } from '@3rdweb/hooks';
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import toast, { Toaster } from 'react-hot-toast';
-import { client } from '../lib/sanityClient';
-
-
+import Head from 'next/head'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import { useWeb3 } from '@3rdweb/hooks'
+import { useEffect } from 'react'
+import { client } from '../lib/sanityClient'
+import toast, { Toaster } from 'react-hot-toast'
 
 const style = {
   wrapper: ``,
-  walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42]`,
+  walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42] `,
   button: `border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
   details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
 }
-
 
 export default function Home() {
   const { address, connectWallet } = useWeb3()
