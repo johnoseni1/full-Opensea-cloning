315453d5940d02e6476b3e8822fbf630bb093d6e
John Oseni <johnoseni26@gmail.com>
2022-02-08 20:30:08 +0100

    index.js file

--- pages/index.js
+++ pages/index.js
@@ -4,3 +4,4 @@ import Head from 'next/head';
 import { useWeb3 } from '@3rdweb/hooks';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import toast, { Toaster } from 'react-hot-toast';
