cb7905192e3a60c0a0c66f1ed21b9380c286161e
John Oseni <johnoseni26@gmail.com>
2022-02-08 20:31:18 +0100

    index.js file

--- pages/index.js
+++ pages/index.js
@@ -6,3 +6,12 @@ import Header from '../components/Header';
 import Hero from '../components/Hero';
 import toast, { Toaster } from 'react-hot-toast';
 import { client } from '../lib/sanityClient';
+
+
+
+const style = {
+  wrapper: ``,
+  walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42]`,
+  button: `border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
+  details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
+}
