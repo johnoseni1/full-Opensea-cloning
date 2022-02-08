9bd090d78a5b04f14303dab786389600c0db22ed
John Oseni <johnoseni26@gmail.com>
2022-02-08 20:39:41 +0100

    index.js file

--- static/events.js
+++ static/events.js
@@ -1,27 +1,27 @@
 export const Events = [
     {
       price: 17,
-      from: '0x',
+      from: '0xjohn',
       to: '0xDavid',
-      date: '4 months ago',
+      date: '2 days ago',
     },
     {
       price: 17,
       from: '0xjohn',
       to: '0xDavid',
-      date: '4 months ago',
+      date: '2 days ago',
     },
     {
       price: 17,
       from: '0xjohn',
       to: '0xDavid',
-      date: '4 months ago',
+      date: '2 days ago',
     },
     {
       price: 17,
       from: '0xjohn',
       to: '0xDavid',
-      date: '4 months ago',
+      date: ' ago',
     },
   ]
   
